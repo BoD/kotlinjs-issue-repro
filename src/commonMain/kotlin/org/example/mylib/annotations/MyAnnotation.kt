36473629214a3e77f7ef6966b073d2fae47d420f
BoD <BoD@JRAF.org>
2022-08-04 10:42:34 +0200

    Initial commit

---
+++ src/commonMain/kotlin/org/example/mylib/annotations/MyAnnotation.kt
@@ -0,0 +1,13 @@
+package org.example.mylib.annotations
+
+@Target(
+    AnnotationTarget.CLASS,
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY,
+    AnnotationTarget.ANNOTATION_CLASS,
+    AnnotationTarget.CONSTRUCTOR,
+    AnnotationTarget.PROPERTY_SETTER,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.TYPEALIAS)
+@Retention(AnnotationRetention.SOURCE)
+annotation class MyAnnotation()
